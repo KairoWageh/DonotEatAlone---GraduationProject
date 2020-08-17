e621209791280207f00bf616f1b15228b4f941d0
KairoWageh <kairo.wageh@gmail.com>
2020-08-17 19:01:25 +0200

    Notifications

--- app/Notification.php
+++ app/Notification.php
@@ -12,35 +12,22 @@ class Notification extends Model
     protected $table ="notifications";
 
     protected $fillable= [
-
-        'IsRead',  'ReservationDate', 'ReservationStartTime','ReservationEndTime', 'NotificationToId1','NotificationToId2', 'NotificationFormId'
-
+        'IsRead',  'ReservationDate', 'ReservationStartTime','ReservationEndTime', 'notifiable_id', 'notifiable_type'
     ];
 
     protected $appends= ['to','from'];
 
-
     public function getCreatedAtAttribute($value){
-
-
-
         return Carbon::parse($value)->diffForHumans();
-
-
     }
 
     public function getToAttribute(){
-
         return User::where('id',$this->NotificationToId)->first();
-
-
     }
 
     public function getFromAttribute(){
-
-
         return User::where('id',$this->NotificationFormId)->first();
-
     }
 
 }
+
