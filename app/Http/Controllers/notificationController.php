31bfb4206424f2045ea0bc14fa239ba32c21f6f8
KairoWageh <kairo.wageh@gmail.com>
2020-08-19 20:19:11 +0200

    Notifications

--- app/Http/Controllers/notificationController.php
+++ app/Http/Controllers/notificationController.php
@@ -6,28 +6,42 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use Illuminate\Support\Facades\DB;
+use App\Notification;
 
 class notificationController extends Controller
 {
     //Carbon::parse($value)->diffForHumans();
-    public function getNotification(Request $request){
+    // public function getNotification(Request $request){
 
-        $notification=DB::table('notifications')
-            ->join('users', function ($join) {
-                $join->on('users.id', '=', 'notifications.NotificationToId1')
-                    ->oron('users.id', '=', 'notifications.NotificationToId2')  ;
-            })
-            ->join('restaurants','restaurants.id','=','NotificationFormId')
+    //     $notification=DB::table('notifications')
+    //         ->join('users', function ($join) {
+    //             $join->on('users.id', '=', 'notifications.NotificationToId1')
+    //                 ->oron('users.id', '=', 'notifications.NotificationToId2')  ;
+    //         })
+    //         ->join('restaurants','restaurants.id','=','NotificationFormId')
 
-          //  ->join('users','NotificationToId1','=','users.id')
-          //  ->join('users','NotificationToId2','=','users.id')
-            ->select('notifications.*','restaurants.RestaurantName','users.UserName')
-            ->get();
+    //       //  ->join('users','NotificationToId1','=','users.id')
+    //       //  ->join('users','NotificationToId2','=','users.id')
+    //         ->select('notifications.*','restaurants.RestaurantName','users.UserName')
+    //         ->get();
 
-            dd($notification);
+    //         dd($notification);
 
 
 
+    // }
+
+    /**
+     * Mark the notification as read.
+     *
+     * @return void
+     */
+    public function markAsRead($id)
+    {
+        $notification = Notification::where('id', $id)->first();
+        if (is_null($notification->read_at)) {
+            $notification->forceFill(['read_at' => $notification->freshTimestamp()])->save();
+        }
+        //return back();
     }
-
 }
