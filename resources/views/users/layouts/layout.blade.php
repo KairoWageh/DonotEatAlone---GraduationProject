23a42dd58e01d7aad9984b4ede26549aca93182a
KairoWageh <kairo.wageh@gmail.com>
2020-08-20 12:19:38 +0200

    Notifications

--- resources/views/users/layouts/layout.blade.php
+++ resources/views/users/layouts/layout.blade.php
@@ -101,40 +101,13 @@
                                     <!-- END LANGUAGE DROPDOWN -->
 
                                     <!-- BEGIN NOTIFICATION DROPDOWN -->
-                                    <!-- <li class="dropdown dropdown-extended dropdown-notification" id="header_notification_bar"> -->
-                                        <!-- <a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="true">
-                                        <i class="icon-bell"></i>
-                                        <span class="badge badge-default"> 7 </span>
-                                        </a>
-                                        <ul class="dropdown-menu">
-                                        </ul> -->
-                                        <!-- <a href="#notifications-panel" class="dropdown-toggle" data-toggle="dropdown">
-                                            <i data-count="0" class="glyphicon glyphicon-bell notification-icon"></i>
-                                        </a>
-                                        <div class="dropdown-container">
-                                        <div class="dropdown-toolbar">
-                                            <div class="dropdown-toolbar-actions">
-                                                <a href="#">Mark all as read</a>
-                                            </div>
-                                            <h3 class="dropdown-toolbar-title">Notifications (<span class="notif-count">0</span>)</h3>
-                                        </div>
-                                        <ul class="dropdown-menu">
-                                        </ul>
-                                        <div class="dropdown-footer text-center">
-                                            <a href="#">View All</a>
-                                        </div>
-                                      </div>
-                                    </li> -->
-                                    <!-- END NOTIFICATION DROPDOWN -->
-
-                                    <!-- BEGIN NOTIFICATION DROPDOWN -->
                                     <li class="dropdown dropdown-extended dropdown-notification" id="header_notification_bar">
                                     <a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="true">
                                         
                                     <i class="icon-bell"></i>
-                                    @if(auth()->user()->unreadNotifications->count() > 0)
-                                        <span class="badge badge-default"> {{ auth()->user()->unreadNotifications->count() }}  </span>
-                                    @endif
+                                    <!-- @if(auth()->user()->unreadNotifications->count() > 0) -->
+                                        <span class="badge badge-default" id="noti_number">   </span>
+                                    <!-- @endif -->
                                     </a>
                                     <ul class="dropdown-menu">
                                         <li class="external">
@@ -394,7 +367,7 @@
                     {!! Html::script('public/assets/global/plugins/jquery.min.js') !!}
                     {!! Html::script('public/assets/global/plugins/bootstrap/js/bootstrap.min.js') !!}
                     {!! Html::script('public/assets/global/plugins/js.cookie.min.js') !!}
-                    {!! Html::script('public/assets/global/plugins/bootstrap-hover-dropdown/bootstrap-hover-dropdown.min.js') !!}
+                    <!-- {!! Html::script('public/assets/global/plugins/bootstrap-hover-dropdown/bootstrap-hover-dropdown.min.js') !!} -->
                     {!! Html::script('public/assets/global/plugins/jquery-slimscroll/jquery.slimscroll.min.js') !!}
                     {!! Html::script('public/assets/global/plugins/jquery.blockui.min.js') !!}
                     {!! Html::script('public/assets/global/plugins/bootstrap-switch/js/bootstrap-switch.min.js') !!}
@@ -449,7 +422,30 @@
                     {!! Html::script('public/assets/layouts/layout/scripts/pusher.min.js') !!}
                     {!! Html::script('public/assets/layouts/layout/scripts/chat.js') !!}
                     <!-- END CAHT SCRIPTS -->
+
+                    <!-- get notifications count -->
                     <script type="text/javascript">
+                        function loadDoc() {
+                            setInterval(function(){
+                                $.ajax({
+                                    type:'GET',
+                                    url:'getNotificationNumber',
+                                    data:{ "_token":  "{{ csrf_token() }}"},
+                                    success:function(data) {
+                                        document.getElementById("noti_number").innerHTML = data;
+                                    }
+                                });
+
+                            },1000);
+                        }
+                        loadDoc();
+                    </script>
+
+
+                    <!-- mark one notification as read -->
+
+
+                    <!-- <script type="text/javascript">
                         $(".notification").click(function(){
                             // clicked notification id
                             var notificationID = this.id;
@@ -462,56 +458,20 @@
                                 }
                             });
                         });
+                    </script> -->
+
+                    <!-- mark all notifications as read -->
+                    <script type="text/javascript">
+                        $("#header_notification_bar").click(function(){
+                            $.ajax({
+                                type:'GET',
+                                url:'markAsRead/',
+                                data:{ "_token":  "{{ csrf_token() }}"},
+                                // success:function(data) {
+                                //     console.log('clicked')
+                                // }
+                            });
+                        });
                     </script>
-                    <!-- <script type="text/javascript">
-                        var notificationsWrapper   = $('.dropdown-notifications');
-                        var notificationsToggle    = notificationsWrapper.find('a[data-toggle]');
-                        var notificationsCountElem = notificationsToggle.find('i[data-count]');
-                        var notificationsCount     = parseInt(notificationsCountElem.data('count'));
-                        var notifications          = notificationsWrapper.find('ul.dropdown-menu');
-
-                        if (notificationsCount <= 0) {
-                            notificationsWrapper.hide();
-                        }
-
-                        // Enable pusher logging - don't include this in production
-                        // Pusher.logToConsole = true;
-
-                        var pusher = new Pusher('API_KEY_HERE', {
-                            encrypted: true
-                        });
-
-                        // Subscribe to the channel we specified in our Laravel Event
-                        var channel = pusher.subscribe('notification');
-
-                        // Bind a function to a Event (the full Laravel class)
-                        channel.bind('App\\Events\\NewNotification', function(data) {
-                            var existingNotifications = notifications.html();
-                            var avatar = Math.floor(Math.random() * (71 - 20 + 1)) + 20;
-                            var newNotificationHtml = `
-                            <li class="notification active">
-                                <div class="media">
-                                    <div class="media-left">
-                                        <div class="media-object">
-                                            <img src="https://api.adorable.io/avatars/71/`+avatar+`.png" class="img-circle" alt="50x50" style="width: 50px; height: 50px;">
-                                        </div>
-                                    </div>
-                                    <div class="media-body">
-                                        <strong class="notification-title">`+data.message+`</strong>
-                                        <!--p class="notification-desc">Extra description can go here</p-->
-                                        <div class="notification-meta">
-                                            <small class="timestamp">about a minute ago</small>
-                                        </div>
-                                    </div>
-                                </div>
-                            </li>`;
-                            notifications.html(newNotificationHtml + existingNotifications);
-
-                            notificationsCount += 1;
-                            notificationsCountElem.attr('data-count', notificationsCount);
-                            notificationsWrapper.find('.notif-count').text(notificationsCount);
-                            notificationsWrapper.show();
-                        });
-                    <!-- </script> --> 
                 </body>
             </html>
